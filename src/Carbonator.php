8da54355d8923947d5081ed4fa0f351c25de2266
bjuppa <nilsved@gmail.com>
2016-08-05 15:21:35 +0200

    parseToTz

--- src/Carbonator.php
+++ src/Carbonator.php
@@ -1,8 +1,35 @@
 <?php
 namespace FewAgency\Carbonator;
 
+use Carbon\Carbon;
+use DateTime;
+use DateTimeZone;
 
 class Carbonator
 {
+    /**
+     * @param string|DateTime $input
+     * @param string|DateTimeZone $tz_target
+     * @param string|DateTimeZone $tz_parse
+     * @return Carbon|null
+     */
+    public static function parseToTz($input, $tz_target = null, $tz_parse = null)
+    {
+        // Parse in target timezone if parse timezone not specified
+        $tz_parse = $tz_parse ?: $tz_target;
 
+        if ($input instanceof DateTime) {
+            $input = Carbon::instance($input);
+        } elseif (strlen($input)) {
+            try {
+                $input = Carbon::parse($input, $tz_parse);
+            } catch (\Exception $e) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+
+        return $input->tz($tz_target);
+    }
 }
---
+++ tests/CarbonatorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use FewAgency\Carbonator\Carbonator;
+use Carbon\Carbon;
+
+class CarbonatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testParseToTzReturnsNullOnFail()
+    {
+        $this->assertNull(Carbonator::parseToTz('fail'));
+    }
+
+    public function testParseToTzReturnsNullOnEmptyString()
+    {
+        $this->assertNull(Carbonator::parseToTz(''));
+    }
+
+    public function testParseToTz()
+    {
+        $c = Carbonator::parseToTz('tomorrow 13:37');
+
+        $this->assertEquals('13:37:00', $c->toTimeString());
+        $this->assertTrue($c->utc);
+    }
+
+    public function testParseToTzWithTargetTz()
+    {
+        $c = Carbonator::parseToTz('tomorrow 13:37', 'Europe/Stockholm');
+
+        $this->assertEquals('Europe/Stockholm', $c->tzName);
+        $this->assertEquals('13:37:00', $c->toTimeString());
+    }
+
+    public function testParseToTzWithParseTz()
+    {
+        $c = Carbonator::parseToTz('tomorrow 13:37', '-1', 'Europe/Stockholm');
+
+        $this->assertEquals('-01:00', $c->tzName);
+        $this->assertNotEquals('13:37:00', $c->toTimeString());
+    }
+}
